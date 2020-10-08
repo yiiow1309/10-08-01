12ddac63e4c8615ab6434574db0b3efd025f9028
yiiow1309 <55523982+yiiow1309@users.noreply.github.com>
2020-10-08 14:50:34 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callbyvalue
+{
+    class Program
+    {
+        static void Swap(int x, int y)
+        {
+            int temp;
+            temp = x; x = y; y = temp;
+            Console.WriteLine(" Swap: x = {0}, y = {1}", x, y);
+        }
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2;
+            Console.WriteLine("Before: x = {0}, y = {1}", x, y);
+            Swap(x, y);
+            Console.WriteLine(" After: x = {0}, y = {1}", x, y);
+        }
+    }
+}
